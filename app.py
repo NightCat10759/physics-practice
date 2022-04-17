8b78fc4dd3f598826db1379eb738caab58ca4bb1
NightCat10759 <c107151159@nkust.edu.tw>
2022-04-17 14:34:43 +0800

    [UPDATE] To try deploy on heroku

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from linebot.exceptions import (
 from linebot.models import *
 
 #======這裡是呼叫的檔案內容=====
-from Method import *
+from method import *
 #======這裡是呼叫的檔案內容=====
 
 
@@ -44,43 +44,51 @@ def callback():
         abort(400)
     return 'OK'
 
-
+# 歡迎訊息
+@handler.add(MemberJoinedEvent)
+def welcome(event):
+    uid = event.joined.members[0].user_id
+    gid = event.source.group_id
+    profile = line_bot_api.get_group_member_profile(gid, uid)
+    name = profile.display_name
+    message = TextSendMessage(text=f'{name}歡迎加入 這個專案是check物理習題做出來的')
+    line_bot_api.reply_message(event.reply_token, message)
 
 # 處理訊息
-Todo_dict = {}
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text #自己傳的訊息 , 型態為String
-    if  '新增' in msg[0:2]:
-        message = IncreaseTodo(msg[2:6],msg[6:],Todo_dict) #(月日,內容,待辦表)
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '刪除' in msg[0:2]:
-        try:
-            int(msg[7])
-        except ValueError:
-            message = TextSendMessage(text="行數必須為整數，詳細請輸入Help。")
-            line_bot_api.reply_message(event.reply_token, message)
-        try:
-            Todo_dict[msg[2:6]]
-            message = DeleteTodo(msg[2:6],msg[7],Todo_dict) #(月日,第幾個,待辦表)
-            line_bot_api.reply_message(event.reply_token, message)
-        except KeyError:
-            message = TextSendMessage(text="本日沒有輸入資料，詳細請輸入Help。")
-            line_bot_api.reply_message(event.reply_token, message)
-    elif '顯示' in msg[0:2]:
-        try:
-            Todo_dict[msg[2:6]]
-            message = ShowTodo(msg[2:6],Todo_dict) #(月日,待辦表)
-            line_bot_api.reply_message(event.reply_token, message)
-        except KeyError:
-            message = TextSendMessage(text="本日沒有輸入資料，詳細請輸入Help。")
-            line_bot_api.reply_message(event.reply_token, message)
-    elif 'Help' in msg:
-        message = Help_template()
-        line_bot_api.reply_message(event.reply_token, message)
-    else:
-        message = TextSendMessage(text="歡迎使用TODO每日待辦機器人，如果不知道如何使用請輸入Help。")
-        line_bot_api.reply_message(event.reply_token, message)
+    message = event.message.text #自己傳的訊息 , 型態為String
+
+    """
+     介面呼叫UI
+     歡迎介面
+     print 用什麼方式找習題呢
+    """
+    
+
+    """
+    1.章節
+    印出現有章節
+    輸入章節
+    印出全部
+
+    輸入標籤
+    印出標籤
+    """
+
+    """
+    2.習題名稱
+    印出現有習題名稱
+    輸入習題名稱然後print
+    """
+
+    """
+    3.標籤
+    印出現有標籤
+    輸入標前然後print
+    """
+    line_bot_api.reply_message(event.reply_token, message)
+
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
---
+++ method.py
@@ -0,0 +1,9 @@
+
+
+def welcome():
+    
+def input_chapter():
+
+def input_exercise_title():
+
+def input_tag():
